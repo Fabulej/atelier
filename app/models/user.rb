3d77494e56b331c0768afb2f19958775a561b777
Fabulej <fabulej@gmail.com>
2017-11-22 21:00:54 +0100

    add gem oAuth with configuration to sign in with google accounts, add A9n gem to keep google id and key safe

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,20 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :reservations
+
+  devise :omniauthable, omniauth_providers: [:google_oauth2]
+
+
+  def self.from_omniauth(access_token)
+  data = access_token.info
+  user = User.where(email: data['email']).first
+
+  unless user
+    user = User.create(
+     email: data['email'],
+     password: Devise.friendly_token[0,20]
+    )
+  end
+  user
+end
 end
